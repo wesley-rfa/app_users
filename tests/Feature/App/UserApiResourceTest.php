05930bf16ee9ff196e96d33057bb2fb383d1f055
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2024-01-21 23:20:09 -0300

    feat: implementing feature user api resource test

--- tests/Feature/App/UserApiResourceTest.php
+++ tests/Feature/App/UserApiResourceTest.php
@@ -32,15 +32,114 @@ class UserApiResourceTest extends TestCase
         ]);
     }
 
-    public function testBadRequestOnStoreMethod(): void
+    public function testValidationOnStoreMethod(): void
     {
-        $response = $this->withHeaders($this->headers)->post($this->route, [
+        $response = $this->withHeaders($this->headers)->postJson($this->route, [
             'name' => '',
             'email' => '',
             'password' => '',
             'passwordConfirmation' => ''
         ]);
 
-        $response->assertStatus(400);
+        $response->assertStatus(400)->assertJsonFragment(['errorCode' => 1]);
+    }
+
+    public function testSuccessfulCreateUser()
+    {
+        $response = $this->withHeaders($this->headers)->postJson($this->route, [
+            'name' => 'Wesley Ribeiro',
+            'email' => 'wesley_ribeirof@outlook.com',
+            'password' => '123456',
+            'passwordConfirmation' => '123456'
+        ]);
+
+        $response->assertStatus(201)->assertJsonStructure([
+            'success',
+            'data' => [
+                'id',
+                'name',
+                'email',
+                'createdAt',
+                'updatedAt'
+            ],
+            'meta' => [
+                'apiVersion'
+            ]
+        ]);
+    }
+
+    public function testNameValidationOnCreateUser()
+    {
+        $response = $this->withHeaders($this->headers)->postJson($this->route, [
+            'name' => '',
+            'email' => 'wesley_ribeirof@outlook.com',
+            'password' => '123456',
+            'passwordConfirmation' => '123456'
+        ]);
+
+        $response->assertStatus(400)->assertJsonStructure([
+            'success',
+            'data' => [
+                'errorCode',
+                'errorMessage',
+                'errorList' => [
+                    'name'
+                ]
+            ],
+            'meta' => [
+                'apiVersion'
+            ]
+        ]);
+    }
+
+    public function testEmailValidationOnCreateUser()
+    {
+        $response = $this->withHeaders($this->headers)->postJson($this->route, [
+            'name' => 'Wesley Ribeiro',
+            'email' => 'wesley_ribeirof',
+            'password' => '123456',
+            'passwordConfirmation' => '123456'
+        ]);
+
+        $response->assertStatus(400)->assertJsonStructure([
+            'success',
+            'data' => [
+                'errorCode',
+                'errorMessage',
+                'errorList' => [
+                    'email'
+                ]
+            ],
+            'meta' => [
+                'apiVersion'
+            ]
+        ]);
+    }
+
+    public function testPasswordValidationOnCreateUser()
+    {
+        $response = $this->withHeaders($this->headers)->postJson($this->route, [
+            'name' => 'Wesley Ribeiro',
+            'email' => 'wesley_ribeirof@outlook.com',
+            'password' => '',
+            'passwordConfirmation' => '123456'
+        ]);
+
+        $response->dump();
+
+        $response->assertStatus(400)->assertJsonStructure([
+            'success',
+            'data' => [
+                'errorCode',
+                'errorMessage',
+                'errorList' => [
+                    'password',
+                    'passwordConfirmation',
+                ]
+            ],
+            'meta' => [
+                'apiVersion'
+            ]
+        ]);
     }
 }
