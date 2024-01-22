32a68c02d2d111439c475f137f8a3652526157be
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2024-01-22 00:52:12 -0300

    feat: adjusting web routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,44 +7,24 @@ use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         return view('app.user.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('app.user.create');
-    }
-    
-    public function store(Request $request)
-    {
-
     }
 
     public function show(string $id)
     {
         return view('app.user.show', compact('id'));
     }
-    
+
     public function edit(string $id)
     {
         return view('app.user.edit', compact('id'));
-    }
-    
-    public function update(Request $request, string $id)
-    {
-
-    }
-    
-    public function destroy(string $id)
-    {
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'index'])->name('welcome.index');
-Route::resource('users', UserController::class);
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
