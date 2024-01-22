242b49b2762959e4df6f25c2d2edee90e3e90d3a
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2024-01-21 22:53:05 -0300

    feat: implementing test sucessccful index method

--- tests/Feature/App/UserApiResourceTest.php
+++ tests/Feature/App/UserApiResourceTest.php
@@ -12,7 +12,27 @@ class UserApiResourceTest extends TestCase
     ];
     private $route = '/api/users';
 
-    public function testBadRequestOnStore(): void
+    public function testSuccessfulIndexMethod()
+    {
+        $response = $this->withHeaders($this->headers)->get($this->route);
+        $response->assertStatus(200)->assertJsonStructure([
+            'success',
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'email',
+                    'createdAt',
+                    'updatedAt'
+                ]
+            ],
+            'meta' => [
+                'apiVersion'
+            ]
+        ]);
+    }
+
+    public function testBadRequestOnStoreMethod(): void
     {
         $response = $this->withHeaders($this->headers)->post($this->route, [
             'name' => '',
